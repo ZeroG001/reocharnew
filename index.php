58e71188465b4b966221a621b47858067df10c9a
Blayne <blayneholland@outlook.com>
2015-09-17 17:21:50 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<meta meta-equiv="Content-Type" content="text/html;charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<title> Reo Char</title>
+
+		<link rel="stylesheet" type="text/css" href="src/components/bootstrap/dist/css/bootstrap.min.css">
+
+		<style>
+			.navbar {
+				min-height: 90px;
+			}
+			.navbar-brand img {
+				height: 60px;
+			}
+		</style>
+
+	</head>
+
+	<body>
+	  <!-- Chapter One Form the header -->
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#"><img src="photos/brand_REOCharitableFoundation.jpg"></a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">About</a></li>
+		        <li><a href="#">Charities</a></li>
+		        <li><a href="#">Events</a></li>
+		        <li><a href="#">Donate</a></li>
+		        <li><a href="#">Photos</a></li>
+		        <li><a href="#">Contacts</a></li>
+
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</body>
+
+	<script type="text/javascript" src="src/components/jquery/dist/jquery.min.js"></script>
+
+</html>
