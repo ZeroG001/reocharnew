a8972c3833db0e03fd8ce26e9996b3d09cd63e35
Blayne <blayneholland@outlook.com>
2015-09-21 17:26:51 -0400

    first commit new branch. Addeding header

--- index.php
+++ index.php
@@ -9,44 +9,61 @@
 		<style>
 			.navbar {
 				min-height: 90px;
+				background-color: white;
 			}
 			.navbar-brand img {
 				height: 60px;
 			}
+
+			.navbar-brand {
+				height: 90px;
+			}
+
+
 		</style>
 
 	</head>
 
 	<body>
+
 	  <!-- Chapter One Form the header -->
-		<nav class="navbar navbar-default">
-		  <div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#"><img src="photos/brand_REOCharitableFoundation.jpg"></a>
-		    </div>
+		<nav class="navbar navbar-fixed-top navbar-default">
 
-		    <!-- Collect the nav links, forms, and other content for toggling -->
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="container-fluid">
 
-		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="#">About</a></li>
-		        <li><a href="#">Charities</a></li>
-		        <li><a href="#">Events</a></li>
-		        <li><a href="#">Donate</a></li>
-		        <li><a href="#">Photos</a></li>
-		        <li><a href="#">Contacts</a></li>
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
 
-		      </ul>
-		    </div><!-- /.navbar-collapse -->
-		  </div><!-- /.container-fluid -->
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				    <span class="sr-only">Toggle navigation</span>
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+				  </button>
+
+				  <a class="navbar-brand" href="#"><img src="photos/brand_REOCharitableFoundation.jpg"></a>
+
+				</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+				  <ul class="nav navbar-nav navbar-right">
+				    <li><a href="#">About</a></li>
+				    <li><a href="#">Charities</a></li>
+				    <li><a href="#">Events</a></li>
+				    <li><a href="#">Donate</a></li>
+				    <li><a href="#">Photos</a></li>
+				    <li><a href="#">Contacts</a></li>
+
+				  </ul>
+
+				</div><!-- /.navbar-collapse -->
+
+			</div><!-- /.container-fluid -->
+
 		</nav>
+
 	</body>
 
 	<script type="text/javascript" src="src/components/jquery/dist/jquery.min.js"></script>
